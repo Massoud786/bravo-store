ad465e7737f67d0bd87780c1f30e003ccbe76853
Massoud786 <massoud152.homayoun@gmail.com>
2025-11-19 19:19:34 -0800

    Add auto slide functionality

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ const productsBtn = document.querySelector('.products-btn');
 productsBtn.addEventListener('click', () => {
     productItem.classList.toggle('open');
 });
+
+// product carousel auto-slide
+
+const tarck = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".carousel-item");
+
+let currentSlide = 0;
+
+function goToSlide(index) {
+    const slideWidth = slides[0].clientWidth;
+    tarck.style.transform = `translatex(-${index * slideWidth}px)`;
+
+}
+if(tarck && slides.length > 0){
+    setInterval(()=>{
+        currentSlide = (currentSlide + 1) % slides.length;
+        goToSlide(currentSlide);
+    },3000);
+}
